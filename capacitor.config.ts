d7a3e1bf40aa3c0047a9f220a93ae74fad17b57d
ro <ro.michi1129@gmail.com>
2022-03-04 17:17:20 +0900

    npx cap init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.parque.ro.appflow.sandbox',
+  appName: 'appflow-sandbox',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
